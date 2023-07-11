1fdf71bdca339cf4891382d1bb8dae66d356fbd1
Bunny <divyaagrawal0747@gmail.com>
2023-07-12 00:17:24 +0530

    completed API integration of Quote Section

--- app.js
+++ app.js
@@ -25,3 +25,21 @@ function show(s)
     console.log(slideNum)
 }
 //Carousel function--ends
+
+
+//Using Quote API--starts
+const thought=document.querySelector(".thought-content") 
+
+fetch("https://api.quotable.io/quotes/random").then((response)=>response.json()).then((data)=>{
+    console.log(data)
+    console.log(data[0].author)
+    let author=data[0].author
+    let quote=data[0].content
+    thought.innerText="\""+quote+"\"\n-"+author
+
+})
+//Using Quote API--ends
+
+
+//Using News API for carousel window--starts
+let newsMainCards=document.getElementsByClassName(".carousel-card-image")
