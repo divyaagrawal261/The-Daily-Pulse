ad8dfb61c70e0a786efe2c1ca1b4e611d2dc35d4
Bunny <divyaagrawal0747@gmail.com>
2023-07-18 23:46:57 +0530

    Added search by keyword page

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
+let date=new Date();
+let month=date.getMonth()+1;
+let year=date.getFullYear();
+let day=date.getDate();
+let container=document.querySelector(".news-container")
 
-let title="India"
-let url = "https://newsapi.org/v2/everything?q="+title+"&from=2023-06-10&sortBy=publishedAt&apiKey=ed8668a21719482a8d700d96b7a99f61";
 
-fetch(url).then((response)=>response.json()).then((data)=>{
-    console.log(data)
+function search()
+{
+    let newsContainer=document.querySelector(".news-container")
+    let keyword=document.querySelector(".searchByKeyword").value
+    if(keyword=="")
+    {
+        alert("Please Enter a valid Keyword")
+        return
+    }
+    newsContainer.innerHTML=""
+    let url = "https://newsapi.org/v2/everything?q="+keyword+"&from="+year+"-"+(month-1)+"-"+day+"&sortBy=publishedAt&apiKey=ed8668a21719482a8d700d96b7a99f61";
     
-    console.log("My Findings")
-    console.log(data.articles)
-    let arts=data.articles
-    console.log(arts[66])
-    document.querySelector(".picture").style.backgroundImage="url("+arts[66].urlToImage+")"
-    document.querySelector(".picture").innerText=arts[66].description
-    
+    fetch(url).then((response)=>response.json()).then((data)=>{
+        console.log(data)
+        let articles=data.articles
+        let j=0;
+        while(j!=articles.length)
+        {if(articles[j].urlToImage!=null)
+        {
+            let newElement=document.createElement("div")
+            newElement.className="news-card"
+            newElement.url=articles[j].url
+            newElement.innerHTML=`<img src=${articles[j].urlToImage} alt="News Image" class="card-image" /><h2 class="card-headline">${articles[j].title}</h2><a href=${articles[j].url} target="_blank">Read More</a>`
+            container.appendChild(newElement)
+            j++
+            continue
+        }
+        j++
+    }
 })
+}
+// document.querySelectorAll(".news-card").addEventListener("onclick",open(this.url))
+// Object.addEventListener("onclick",open(this.url))
+
+// function open(URL)
+// {
+//     window.open(URL,`_parent`)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
